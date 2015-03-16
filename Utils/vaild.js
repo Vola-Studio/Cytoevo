ecd33d60bdc7fcca6590b5c22b4bee30fe65d18e
Jack Works <zjwpeter@gmail.com>
2015-03-16 20:13:18 +0800

    修正2个break语句缺失导致的错误，修复将data误写成vaild导致的Array检测始终是false的问题，新增了RegExp类型的检测

--- Utils/vaild.js
+++ Utils/vaild.js
@@ -9,15 +9,17 @@ function vaild(data, type){
     var result;
     switch(type){
         case Array:
-            result = Array.isArray(vaild);break;
+            result = Array.isArray(data);break;
         case Object:
-            result = !(data instanceof Array || data instanceof Function || data instanceof String || data instanceof Number) || data === null;break;
+            result = !(vaild(data, Array) || vaild(data, Function) || vaild(data, String) || vaild(data, Number)) || data === null;break;
         case Function:
             result = data instanceof Function;break;
         case Number:
             result = vaild(data, "NaN") || (Number(data) === data);break;
         case String:
             result = data instanceof String;break;
+        case RegExp:
+            result = data instanceof RegExp;break;
         case "Infinity":
         case "inf":
             result = !isFinite(data);break;
@@ -32,9 +34,9 @@ function vaild(data, type){
         case "unsigned Number":
             result = vaild(data, Number) && data >=0;break;
         case "bin":
-            result = (vaild(data, String) && vaild(data.match(/^(0|1)+$/), Array)) || vaild(data, "float");
+            result = (vaild(data, String) && vaild(data.match(/^(0|1)+$/), Array)) || vaild(data, "float");break;
         case "hex":
-            result = (vaild(data, String) && vaild(data.toLowerCase().match(/^(0|1|2|3|4|5|6|7|8|9|a|b|c|d|e|f)+$/), Array)) || vaild(data, "float");
+            result = (vaild(data, String) && vaild(data.toLowerCase().match(/^(0|1|2|3|4|5|6|7|8|9|a|b|c|d|e|f)+$/), Array)) || vaild(data, "float");break;
         case "bool":
             result = (data === true || data === false) || (!!data === true || !!data === false);break;
         case "undefined":
