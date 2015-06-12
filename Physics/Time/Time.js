9b1f31f2abb66757d0df9483e4faa6ff8967cdf6
Jack Works <zjwpeter@gmail.com>
2015-06-12 18:43:16 +0800

    Time Vaild

--- Physics/Time/Time.js
+++ Physics/Time/Time.js
@@ -10,17 +10,32 @@
  * 抛出错误:
  * You can't travel in time!
  *     该值不允许设置
+ *
+ * Time_Pause_Status_Changed [Event] [bool ifRunning]
+ *     当暂停状态改变时产生的事件
+ *     bool ifRunning是时间在改变后的状态,true为正常
  */
-var vaild = require("../../Utils/vaild.js");
+var event = require("../../Event/Event.js");
+
+var flag = event.flags.Physics_Time = {
+    Time_Paused: Math.random().toString(),
+    Time_Resumed: Math.random().toString(),
+    Time_Pause_Status_Changed: Math.random().toString()
+}
+
 //时间是否暂停
 var ifTimePaused = false;
+
 //从世界被建立开始经过的时间总数
 var tick = 0;
 
 module.exports = {
     //获取和设置Time的暂停状态
     get pause(){return ifTimePaused;},
-    set pause(val){ifTimePaused = !!val;},
+    set pause(val){
+        ifTimePaused = !!val;
+        event.publish(flag.Time_Pause_Status_Changed, [!!val]);
+    },
 
     //获取和设置Time的Tick数
     get tick(){return tick;},
