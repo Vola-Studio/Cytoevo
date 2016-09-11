f1c65ab419284a36163710bdc89012eda7f2b858
Jack Works <zjwpeter@gmail.com>
2016-09-12 07:55:05 +0800

    Fix class call problem

--- Biome/Compiler/compiler.js
+++ Biome/Compiler/compiler.js
@@ -20,6 +20,7 @@ const saveFunction = (state, object) => {
 }
 const funcType = pattern => ({'1100': 'out', '1110': 'in'}[pattern])
 const eventType = a => a
+const bin = x => parseInt(x, 2)
 
 const compiler_v2_x16_helper = (raw, object) => 
 	raw.match(/.{16}/g)
@@ -34,28 +35,27 @@ const compiler_v2_x16_helper = (raw, object) =>
 						state.data = []
 						return ''
 					}
-					case '0010': return id => total => `this.generateEnergy(${id}, ${total})`
-					case '0101': return id => total => `this.require(${id}, ${total})`
-					case '0110': return id => total => `this.provide(${id}, ${total})`
+					case '0010': return id => total => `this.generateEnergy(${bin(id)}, ${bin(total)})`
+					case '0101': return id => total => `this.require(${bin(id)}, ${bin(total)})`
+					case '0110': return id => total => `this.provide(${bin(id)}, ${bin(total)})`
 					case '1001': return _ => 'this.divide()'
 					case '0000':
-					case '0001': return queryType => wantedValue => operator => `if(this.query(${queryType}, ${wantedValue}, ${operator}))`
+					case '0001': return queryType => wantedValue => operator => `if(this.query(${bin(queryType)}, ${bin(wantedValue)}, ${operator}))`
 					default:     return _ => ''
 				}
 			} else if (index == array.length - 1) {
 				let val = curriedPatternApply(pattern)
 				state.data.push(val)
-				console.log(val)
 				return state
 			} else return curriedPatternApply(pattern)
 		}, new Function())
 	, {
-		lastFunction: null,
+		lastFunction: ['1110', 'now'],
 		data: []
 	})
 class compiler_v2_x16 {
 	constructor (code) {
-		let _code = code
+		let _code
 		Object.defineProperty(this, 'code', {get: function () {
 			return _code
 		}, set (value) {
@@ -64,12 +64,12 @@ class compiler_v2_x16 {
 			//       触发后再重编译
 			this.compiler()
 		}})
-
+		this.code = code
 		this.bits = 16
 		this.version = 2
 	}
 	compiler () {
-		compiler_v2_x16_helper(this, this.code + '1100000000000000')
+		compiler_v2_x16_helper(this.code + '1100000000000000', this)
 	}
 }
 export default compiler_v2_x16
