2e52ab39fe44a37f404466db40e3a23ced178bb4
Jack Works <zjwpeter@gmail.com>
2016-09-12 08:07:29 +0800

    Time update

--- Physics/Time/Time.js
+++ Physics/Time/Time.js
@@ -12,31 +12,21 @@
  *     当暂停状态改变时产生的事件
  *     bool ifRunning是时间在改变后的状态,true为正常
  */
-var event = require("../../Event/Event.js");
 
-var flag = event.flags.Physics_Time = {
-    Time_Paused: Math.random().toString(),
-    Time_Resumed: Math.random().toString(),
-    Time_Pause_Status_Changed: Math.random().toString()
+//var event = require("../../Event/Event.js");
+
+const flag = /*event.flags.Physics_Time =*/ {
+	Time_Paused: symbol(),
+	Time_Resumed: symbol(),
+	Time_Pause_Status_Changed: symbol(),
 }
 
-//时间是否暂停
-var ifTimePaused = false;
-
-//从世界被建立开始经过的时间总数
-var tick = 0;
-
-module.exports = {
-    //获取和设置Time的暂停状态
-    get pause(){return ifTimePaused;},
-    set pause(val){
-        ifTimePaused = !!val;
-        event.publish(flag.Time_Pause_Status_Changed, [!!val]);
-    },
-
-    //获取和设置Time的Tick数
-    get tick(){return tick;},
-    set tick(val){
-        
-    }
+class Time {
+	constructor () {
+		this.tick = 0
+		this.pause = false
+	}
+	refresh () {return new Time()}
 }
+
+export default new Time()
