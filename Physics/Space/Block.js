45ffd9a91b8e9c91c49624b1f175cbcfe50f5a93
Jack Works <zjwpeter@gmail.com>
2016-09-12 08:09:02 +0800

    Block module

--- Physics/Space/Block.js
+++ Physics/Space/Block.js
@@ -1,22 +1,8 @@
-/* Block 生成单位的类
- *
- * Block(opt) [private] 生成一个单位
- * 参数:
- * new Block(Object opt{
- *     unsigned int x,
- *     unsigned int y
- * })
- *
- */
-function Block(opt){
-    this.x = opt.x;
-    this.y = opt.y;
-    this.biomes = [,,,];
-
-    //TODO 对此单位分配物质
-    //TODO 对此单位分配能量
-        //TODO 根据能量对此单位设定温度
-    
+export class Block {
+	constructor (opt) {
+		this.x = opt.x
+		this.y = opt.y
+		this.biomes = [,,,] // at most 4
+		// TODO: Set matter and energy
+	}
 }
-
-module.exports = Block;
