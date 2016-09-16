4b8cf61b3b07506b504503e946251255de1a0c4c
Jack Works <zjwpeter@gmail.com>
2016-09-16 22:28:33 +0800

    Event module

--- Event/Event.js
+++ Event/Event.js
@@ -1,73 +1,64 @@
-/** @module
- * @file Event controllers.
- * @requires ../Utils/format.js
- * @requires ../Utils/valid.js
- * @copyright zjwpeter 2014-2015 <zjwpeter@gmail.com>
- * @license GPL-3.0+
- */
-
-require("../Utils/format.js");
-
-// TODO: Deprecate this.
-function console_red(a,b){
-    console.error("\x1B[31m", a, b, "\x1B[39m");
+let symbs = {_: Symbol()}
+let anonymous = Symbol()
+let sets = {
+	shouldHave: symb => sets[symb] = sets[symb] || {[anonymous]: []}
 }
 
-/** Event Pool. @private */
-var pool = {};
-/** Event Flags. @static
-* Please follow the `Event.flags.Module_Name.Flag_Name` scheme. */
-var flags = {};
-
-/**
-* Suscribes for a event.
-*
-* @param {string} name - Event Name.
-* @param {string} fKey - Function name key.
-* @param {function} fn - The function to execute in publish.
-* @throws {TypeError}    All type mismatch.
-*/
-function on(name, fnName, fn){
-    if(!pool[name]){
-        pool[name] = {};
-    }
-    pool[name][fnName] = fn;
+let center = new class {
+	when (symb, fn, fnName = Symbol()) {
+		sets.shouldHave(symb)
+		if(fnName.constructor === Symbol) {
+			sets[symb][anonymous].push(fn)
+			return sets[symb][anonymous].length - 1
+		}
+		else {
+			sets[symb][fnName] = fn
+		}
+	}
+	once (symb, fn) {
+		let promFn
+		let prom = new Promise((resolve, reject) => promFn = {resolve, reject})
+		sets.shouldHave(symb)
+		let fnName = this.when(symb, function (event) {
+			let r = pub(fn, event)
+			!(r instanceof Error) ? promFn.resolve(r) : promFn.reject(e)
+			this.remove(symb, fnName)
+		})
+		return prom
+	}
+	remove (symb, fnName) {
+		sets.shouldHave(symb)
+		if(Number.isInterger(fnName)) sets[symb][anonymous][length] = undefined
+		else delete sets[symb][fnName]
+	}
 }
 
-/**
-* Publishs a event.
-*
-* @param {string} name - Event Name.
-* @param {string} args - Argument to pass to each function.
-* @param         _this - The `this` context for the functions.
-* @throws {TypeError}    All type mismatch.
-* @throws Everything thrown by event processors.
-*/
-function publish(name, args, _this){
-    for(var i in pool[name]){
-        _this = _this ? null : _this;
-        pool[name][i].apply(_this, args);
-    }
+function pub (fn, env) {
+	if (fn instanceof Function) try {
+		return fn.bind(env)(env, ...env.data)
+	} catch (e) {
+		return e
+	}
 }
 
-/**
-* Cancels subscrption
-*
-* @param {string} name - Event Name.
-* @param {string} fKey - Function name key.
-* @throws {TypeError}    All type mismatch.
-* @return {Boolean}      False on failure (e.g. non-exist) and otherwise true.
-*/
-function cancel(name, fKey){
-    try{
-        return delete pool[name][fKey];
-    }catch(e){
-        return false;
-    }
+class Event {
+	constructor (symb, ...data) {
+		// Todo: this.tick = now time
+		this.data = data
+		this.symb = symb
+	}
+	dispatch () {
+		let set = sets[this.symb]
+		if (!set) return false
+
+		let cancel = false
+		this.prevent = () => cancel = false
+		debugger
+
+		for (let name in set) cancel || pub(set[name], this)
+		set[anonymous].forEach(f => cancel || pub(f, this))
+	}
 }
-module.exports = {
-    on: on,
-    publish: publish,
-    cancel: cancel,
-    flags: flags
-}
+
+let start = center.once(symbs._, (e, d) => console.log('Event init: ' + d))
+new Event(symbs._, Date()).dispatch()
